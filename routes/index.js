b9615a9c93008d38515a0cf6176b27d4539a64f2
Richard Cyrus <richard.cyrus@rcyrus.com>
2019-02-11 00:24:53 -0500

    Make titles, navbar, and scripts dynamic
    - Update Jumbotron to show different subtitles based on the route
    - Update the Navigation bar to show different options based on the route
    - Update the scripts that are loaded based on the route called

--- routes/index.js
+++ routes/index.js
@@ -10,7 +10,21 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
-    res.render('index', { csrfMeta: req.csrfToken(), title: 'News Scraper' });
+    res.render('index', {
+        csrfMeta: req.csrfToken(),
+        title: 'News Scraper',
+        subtitle: "All the News that's Fit to Scrape",
+        indexRoute: true,
+    });
+});
+
+router.get('/saved', (req, res) => {
+    res.render('index', {
+        csrfMeta: req.csrfToken(),
+        title: 'News Scraper',
+        subtitle: 'Your Saved Articles',
+        savedRoute: true,
+    });
 });
 
 module.exports = router;
